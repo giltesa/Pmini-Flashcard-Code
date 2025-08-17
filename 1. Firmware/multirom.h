cae6163984462704346f952691bbbfc61252fa6d
Alberto Gil Tesa <developer@giltesa.com>
2025-08-17 21:43:32 +0200

    Base firmware, commit e400474db2f5116489b1f8927e075dedddc1f417

--- 1. Firmware/multirom.h
+++ 1. Firmware/multirom.h
@@ -1,7 +1,4 @@
-#define NUM_GAMES 2
-#define GAME_SIZE 524288
-
-const uint8_t rom[ NUM_GAMES * GAME_SIZE ] __attribute__ ((section(".romStorage"))) = {
-    'R','O','M','S','T','A','R','T',
-    // The rest is automatically initialized to zero by the compiler
-};
+// For a 16MB Flash, use the larger declaration.
+const uint8_t rom[ 2 * 524288 ] __attribute__ ((section(".romStorage"))) = {
+//const uint8_t rom[ 20 * 524288 ] __attribute__ ((section(".romStorage"))) = {
+  'R', 'O', 'M', 'S', 'T', 'A', 'R', 'T' };
